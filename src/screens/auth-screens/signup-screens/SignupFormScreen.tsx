82795aeb474280b53e9f0c25bc3b042f97d93e52
Unknown-Coder769 <unknowncoder769@gmail.com>
2023-08-26 03:23:20 +0500

    sign up scroll fixed

--- src/screens/auth-screens/signup-screens/SignupFormScreen.tsx
+++ src/screens/auth-screens/signup-screens/SignupFormScreen.tsx
@@ -14,12 +14,11 @@ import {
 import HeaderBackArrow from '../../../components/shared-components/HeaderBackArrow';
 import {STYLES} from '../../../styles/globalStyles';
 import SignupForm from '../../../components/auth-components/signup/SignupForm';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const SignupFormScreen = ({navigation}: any) => {
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="always"
-      showsVerticalScrollIndicator={false}>
+    <KeyboardAwareScrollView>
       <ImageBackground
         source={require('../../../../assets/images/backgroundImage2.jpg')}
         style={{flex: 1}}
@@ -46,7 +45,7 @@ const SignupFormScreen = ({navigation}: any) => {
           {/* </BlurView> */}
         </View>
       </ImageBackground>
-    </ScrollView>
+    </KeyboardAwareScrollView>
   );
 };
 
